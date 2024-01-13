4d714cfbc8aa3aae4bc2fde3e31888789fc288f4
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2024-01-13 09:47:39 +0100

    Initial navbar styling

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -1,35 +1,47 @@
-import Image from "next/image";
-import Link from "next/link";
+import Image from 'next/image'
+import Link from 'next/link'
 
-export default function Navbar() { 
+export default function Navbar() {
   return (
     <nav>
-      <div>
-        <Link href="/">
-          <Image src="/logo.svg" alt="Logo" width={62} height={62} />
-        </Link>
-      </div>
-      <div>
-        <Link href="/about">
-          <h3 className="text-aubergine-500">About</h3>
-        </Link>
-      </div>
-      <div>
-        <Link href="/cases">
-          <h3 className="text-aubergine-500">Cases</h3>
-        </Link>
-      </div>
-      <div>
-        <Link href="/faq">
-          <h3 className="text-aubergine-500">FAQ</h3>
-        </Link>
-      </div>
-      <div>
-        <Link href="/kontakt">
-          <button className="text-aubergine-500 px-12 py-3 border-aubergine-500 border-solid border-2 rounded-2xl hover:bg-aubergine-500 hover:text-apricot-500">
-            <h3>Kontakt</h3>
-          </button>
-        </Link>
+      <div className="px-11 py-4 flex justify-between border-b-2 border-salmon-500 gap-16">
+        <div className="flex items-center">
+          <Link href="/">
+            <Image src="/logo.svg" alt="Logo" width={62} height={62} />
+          </Link>
+        </div>
+
+        <div className="flex items-center gap-16">
+          <div>
+            <Link href="/about">
+              <h4 className="text-aubergine-500">About</h4>
+            </Link>
+          </div>
+
+          <div>
+            <Link href="/cases">
+              <h4 className="text-aubergine-500">Cases</h4>
+            </Link>
+          </div>
+
+          <div>
+            <Link href="/faq">
+              <h4 className="text-aubergine-500">FAQ</h4>
+            </Link>
+          </div>
+
+          <div>
+            <Link href="/kontakt">
+              <button className="text-aubergine-500 px-12 py-3 border-aubergine-500 border-solid border-2 rounded-2xl hover:bg-aubergine-500 hover:text-apricot-500">
+                <h4>Kontakt</h4>
+              </button>
+            </Link>
+          </div>
+
+          <div>
+            <Image src="/hamburger.svg" alt="Menu" width={18} height={18} />
+          </div>
+        </div>
       </div>
     </nav>
   )
