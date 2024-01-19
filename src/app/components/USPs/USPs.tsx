e18f152a9e56e8447f67e9ef2d92f7d29b3a274e
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2024-01-19 13:58:17 +0100

    Add USPs section to home

---
+++ src/app/components/USPs/USPs.tsx
@@ -0,0 +1,18 @@
+export default function USPs() { 
+  return (
+    <section className="grid grid-cols-3 gap-4">
+      <div className="pl-10 py-20 border-2 border-apricot-500 border-r-salmon-500 h-full">
+        <h4 className="text-salmon-500 pb-5">01.</h4>
+        <h2 className="text-aubergine-500">Sortiment &<br />Inköp</h2>
+      </div>
+      <div className="pl-10 py-20 border-2 border-apricot-500 border-r-salmon-500 h-full">
+        <h4 className="text-salmon-500 pb-5">02.</h4>
+        <h2 className="text-aubergine-500">Kvalitet &<br />hållbarhet</h2>
+      </div>
+      <div className="pl-10 py-20">
+        <h4 className="text-salmon-500 pb-5">03.</h4>
+        <h2 className="text-aubergine-500">Försäljning &<br />marknad</h2>
+      </div>  
+    </section>
+  )
+}
