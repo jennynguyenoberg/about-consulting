767d6acd25bf5bed767882493f2354d8ca10a565
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2024-01-19 12:58:00 +0100

    Update Hero section + bkg image

--- src/app/components/Hero/Hero.tsx
+++ src/app/components/Hero/Hero.tsx
@@ -3,17 +3,10 @@ import Image from "next/image";
 export default function Hero() { 
   return (
     <div>
-      <section>
-        <Image
-          src="/bkg.svg"
-          alt="Background"
-          width={1500}
-          height={62}
-          className="relative w-full z-0 border-b-2 border-salmon-500"
-          />
-        <div className='mx-48 absolute bottom-44 text-aubergine-500 flex flex-col justify-items-center'>
+      <section className="h-screen flex justify-center items-center bg-[url('/bkg.svg')] border-b-2 border-salmon-500">
+        <div>
           <div className="flex gap-7">
-            <h1>About</h1>
+            <h1 className='text-aubergine-500'>About</h1>
             <h1 className="text-salmon-500">Consulting</h1>
           </div>
         </div>
