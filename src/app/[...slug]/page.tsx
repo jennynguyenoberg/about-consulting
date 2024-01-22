78a94c28cc55dcebfd869a29f6e055a822492fb6
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2024-01-22 20:48:27 +0100

    Add 404 page

---
+++ src/app/[...slug]/page.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link"
+
+export const metadata = {
+  title: 'Page does not exist',
+  // description: "",
+}
+
+export default function NotFoundPage() { 
+  return (
+    <main className='py-60 px-14 h-full text-aubergine-500'>
+      <h1>404</h1>
+      <h2>Page does not exist</h2>
+      <p>Go back to <span className="underline"><Link href='/'>Home</Link></span></p>
+    </main>
+  )
+}
