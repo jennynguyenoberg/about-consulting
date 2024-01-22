08bd5a90a47c0c245bbf7e31302555f4dc2412c0
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2024-01-22 17:12:23 +0100

    Add navigation arrows on image carousel

--- src/app/components/ImageCarousel/ImageCarousel.tsx
+++ src/app/components/ImageCarousel/ImageCarousel.tsx
@@ -47,13 +47,13 @@ export default function ImageCarousel() {
         className='w-full h-full rounded-3xl bg-center bg-cover duration-500'
       ></div>
       {/* Left Arrow */}
-      {/* <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+      <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-salmon-500/40 text-white cursor-pointer'>
         <BsChevronCompactLeft onClick={prevSlide} size={30} />
-      </div> */}
+      </div>
       {/* Right Arrow */}
-      {/* <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-salmon-500/20 text-white cursor-pointer'>
+      <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-salmon-500/40 text-white cursor-pointer'>
         <BsChevronCompactRight onClick={nextSlide} size={30} />
-      </div> */}
+      </div>
       <div className='flex relative bottom-14 justify-center py-0'>
         {slides.map((slide, slideIndex) => (
           <div
