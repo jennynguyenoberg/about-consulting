a01a86ef5bacbda516748cf85aaa887945a15b56
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2024-01-23 10:56:56 +0100

    Final animation on enu button in navbar

---
+++ src/app/components/Navbar/NavItems/data.js
@@ -0,0 +1,41 @@
+export const Links = [
+  {
+    title: "Cases",
+    href: '/'
+  },
+  {
+    title: "Teamet",
+    href: '/'
+  },
+  {
+    title: "Samarbetspartners",
+    href: '/'
+  },
+  {
+    title: "Kontakt",
+    href: '/kontakt'
+  },
+  {
+    title: "Jobb",
+    href: '/'
+  },
+]
+
+export const FooterLinks = [
+  {
+    title: "Instagram",
+    href: '/'
+  },
+  {
+    title: "LinkedIn",
+    href: '/'
+  },
+  // {
+  //   title: "Instagram",
+  //   href: '/'
+  // },
+  // {
+  //   title: "LinkedIn",
+  //   href: '/'
+  // }
+]
--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -4,6 +4,25 @@ import Link from 'next/link'
 import NavbarButton from './NavbarButton/NavbarButton'
 import styles from './Navbar.module.scss'
 import { useState } from 'react'
+import { AnimatePresence, motion } from 'framer-motion'
+import NavItems from './NavItems/NavItems'
+
+const variants = {
+  open: {
+    width: 480,
+    height: 650,
+    top: '-25px',
+    right: '-25px',
+    transition: { duration: 0.75, ease: [0.76, 0, 0.24, 1] }
+  },
+  closed: {
+    width: 40,
+    height: 40,
+    top: '0px',
+    right: '0px',
+    transition: { duration: 0.75, delay: 0.35, ease: [0.76, 0, 0.24, 1] }
+  }
+}
 
 export default function Navbar() {
   const [isActive, setIsActive] = useState(false)
@@ -44,8 +63,19 @@ export default function Navbar() {
               </button>
             </Link>
           </div>
+          <div className='pr-10'/>
 
           <div className={styles.header}>
+            <motion.div
+              className={styles.menu}
+              variants={variants}
+              animate={isActive ? 'open' : 'closed'}
+              initial='closed'
+            >
+              <AnimatePresence>
+                {isActive && <NavItems />}
+              </AnimatePresence>
+            </motion.div>
             <NavbarButton isActive={isActive} setIsActive={setIsActive} />
           </div>
         </div>
