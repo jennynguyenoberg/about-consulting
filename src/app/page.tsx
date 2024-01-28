426bc58174b0da7a0e36b4b14fbc2a6ca70028d2
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2024-01-28 18:16:51 +0100

    Update mobile scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,8 +13,10 @@ export default function Home() {
 
   useEffect(() => {
     ;(async () => {
-      const LocomotiveScroll = (await import('locomotive-scroll')).default
-      const locomotiveScroll = new LocomotiveScroll()
+      if (window.innerWidth > 600) {
+        const LocomotiveScroll = (await import('locomotive-scroll')).default
+        const locomotiveScroll = new LocomotiveScroll()
+      }
 
       setTimeout(() => {
         setIsLoading(false)
