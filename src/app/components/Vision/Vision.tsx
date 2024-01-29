9f51a3d1c8f5ecf5b36f98d2b5f8cd70ed6f2a66
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2024-01-29 21:50:39 +0100

    Final styling tweaks

--- src/app/components/Vision/Vision.tsx
+++ src/app/components/Vision/Vision.tsx
@@ -4,7 +4,7 @@ export default function Vision() {
   return (
     <section>
       <div className="grid grid-cols-1 lg:grid-cols-2 text-aubergine-500 justify-center items-center border-b-2 border-salmon-500">
-        <div className="p-20 lg:py-36 lg:px-28 bg-salmon-500 text-center flex items-center flex-col">
+        <div className="px-10 py-20 lg:py-36 lg:px-28 bg-salmon-500 text-center flex items-center flex-col">
           <Image
             src="/marika_profile.jpg"
             alt="Profile image of Marika Hjorth"
@@ -12,9 +12,9 @@ export default function Vision() {
             height={135}
             className="rounded-full"
           />
-          <p className="text-3xl lg:text-5xl mt-5">
+          <p className="text-2xl lg:text-5xl mt-5">
             "Vi älskar
-            <em className="text-4xl lg:text-5xl">
+            <em className="text-3xl lg:text-5xl">
               {' '}
               att driva affärer inom ett område som vi personligen brinner för
               och About är med dig från idé till lönsamhet.
@@ -25,14 +25,14 @@ export default function Vision() {
         </div>
         <div className="py-20 px-10 lg:px-14 flex gap-8 flex-col">
           <h4>Vårt team</h4>
-          <p className="text-3xl mb-16">
+          <p className="text-xl lg:text-3xl mb-16">
             Mer än ett konsultbolag. Ett team, vars erbjudande är unikt i sin
             klass. Vi har gedigen erfarenhet av FMCG och retail, allt från tuffa
             förhandlingar till hur du syns bäst i butikshyllan. Dessutom
             garanterar vi 100% härlig energi.
           </p>
           <h4>Vårt mål</h4>
-          <p className="text-3xl">
+          <p className="text-xl lg:text-3xl">
             About ska vara den självklara partnern för varumärken inom FMCG och
             retail som vill driva innovation och lönsamma affärer.
           </p>
